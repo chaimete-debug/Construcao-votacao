c747f13dafd85adf0f7e24dad632ab95ff161938
chaimete-debug <chaimete@gmail.com>
2026-04-15 20:40:17 +0200

    Enhance app component with Head for SEO and mobile
    Added meta tags and links for mobile web app capabilities.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Votação" />
+        <meta name="theme-color" content="#7F77DD" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
